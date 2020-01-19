f93f05b641d42f077ddd58782d223c8f5b454821
Alexandros Megalemos <amegalemos1@gmail.com>
2020-01-19 20:06:05 +0000

    asd

--- robotics/catkin_ws/beginner_tutorials/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/meldanen/megus_repo/megus/robotics/catkin_ws/beginner_tutorials/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
