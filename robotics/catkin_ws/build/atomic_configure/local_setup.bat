29069c30331c010c931c754163cd8519a56ea2f4
Alexandros Megalemos <amegalemos1@gmail.com>
2020-01-19 23:23:07 +0000

    deleted cat ws for clean setup

--- robotics/catkin_ws/build/atomic_configure/local_setup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM generated from catkin/cmake/template/local_setup.bat.in
-
-set _CATKIN_SETUP_DIR=F:/KCL/kcl/robotics/catkin_ws/devel
-call %_CATKIN_SETUP_DIR%/setup.sh --extend --local
-set _CATKIN_SETUP_DIR=
--- robotics/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "F:/KCL/kcl/robotics/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "C:/opt/ros/melodic/x64".split(';') if "C:/opt/ros/melodic/x64" != "" else []
