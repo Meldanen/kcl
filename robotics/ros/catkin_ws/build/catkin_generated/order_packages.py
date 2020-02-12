3dea7c5d8e72392238c46d905c351dcfde7f3140
Alexandros Megalemos <amegalemos1@gmail.com>
2020-02-12 18:52:39 +0000

    start assignment 2

---
+++ robotics/ros/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/mikroszolos/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/mikroszolos/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/mikroszolos/catkin_ws/devel;/opt/ros/melodic" != "" else []
